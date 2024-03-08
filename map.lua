8fe30792a6362d293da183ab4d57b7568dfb77d9
The-Sickness <alloycowboy6969@gmail.com>
2024-03-08 11:02:15 -0600

    Applied some fixes

--- map.lua
+++ map.lua
@@ -1,50 +1,75 @@
 local addonName, addonNamespace = ...
 
+addonNamespace = addonNamespace or {}
 
-local function UpdateTeamPlayerIndicators()
-
-    for i = 1, GetNumGroupMembers() do
-        local unit = "party"..i
-        if UnitIsUnit(unit, "player") then
-           
-        else
- 
-            local name, rank, subgroup, level, class, fileName, zone, online, isDead = GetRaidRosterInfo(i)
-            
-        end
+local function EnsureDBSettings()
+    if not IncCalloutDB.settings then
+        IncCalloutDB.settings = {
+            mapScale = 0.75, -- Default scale value
+            mapPosition = { -- Default position settings
+                point = "CENTER",
+                relativePoint = "CENTER",
+                xOfs = 0,
+                yOfs = 0,
+            },
+        }
     end
 end
 
--- Initialize map features and setup event handlers
+local function SaveMapPosition()
+    local point, _, relativePoint, xOfs, yOfs = WorldMapFrame:GetPoint()
+    IncCalloutDB.settings.mapPosition = {
+        point = point,
+        relativePoint = relativePoint,
+        xOfs = xOfs,
+        yOfs = yOfs,
+    }
+end
+
+local function ResizeWorldMap()
+    EnsureDBSettings() 
+    if WorldMapFrame and IncCalloutDB.settings.mapScale then
+        WorldMapFrame:SetScale(IncCalloutDB.settings.mapScale)
+        
+        local pos = IncCalloutDB.settings.mapPosition
+        WorldMapFrame:ClearAllPoints()
+        WorldMapFrame:SetPoint(pos.point, UIParent, pos.relativePoint, pos.xOfs, pos.yOfs)
+    end
+end
+
+WorldMapFrame:HookScript("OnShow", ResizeWorldMap)
+WorldMapFrame:SetMovable(true)
+WorldMapFrame:EnableMouse(true)
+WorldMapFrame:RegisterForDrag("LeftButton")
+WorldMapFrame:SetScript("OnDragStart", WorldMapFrame.StartMoving)
+WorldMapFrame:SetScript("OnDragStop", function()
+    WorldMapFrame:StopMovingOrSizing()
+    SaveMapPosition() 
+end)
+
 local function InitializeMapFeatures()
-     
     local eventFrame = CreateFrame("Frame")
     eventFrame:RegisterEvent("GROUP_ROSTER_UPDATE") 
     eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD") 
     
     eventFrame:SetScript("OnEvent", function(self, event, ...)
         if event == "GROUP_ROSTER_UPDATE" or event == "PLAYER_ENTERING_WORLD" then
-            UpdateTeamPlayerIndicators()
+            
         end
     end)
 end
 
-addonNamespace.InitializeMapFeatures = InitializeMapFeatures
+addonNamespace.ResizeWorldMap = ResizeWorldMap
+InitializeMapFeatures()
 
-local function ResizeWorldMap()
-    if WorldMapFrame then
-        WorldMapFrame:SetScale(0.75)         
-        WorldMapFrame:ClearAllPoints()
-        WorldMapFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
-    end
-end
 
-WorldMapFrame:HookScript("OnShow", ResizeWorldMap)
 
-WorldMapFrame:SetMovable(true)
-WorldMapFrame:EnableMouse(true)
-WorldMapFrame:RegisterForDrag("LeftButton") 
-WorldMapFrame:SetScript("OnDragStart", WorldMapFrame.StartMoving)
-WorldMapFrame:SetScript("OnDragStop", WorldMapFrame.StopMovingOrSizing)
+
+
+
+
+
+
+
 
 
